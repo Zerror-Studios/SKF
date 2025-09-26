15ee254fb4df11fd9ff3f47a670c7d58d1f6855d
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-09-26 14:22:58 +0530

    added upcoming banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import HeroSection from '@/components/home/HeroSection'
 import Highlights from '@/components/home/Highlights'
 import React from 'react'
 import { movies } from '@/helper/moviesData'
-import UpcomingSection from '@/components/home/UpcomingSection'
 import GallerySection from '@/components/common/GallerySection'
+import UpcomingBanner from '@/components/home/upcoming/UpcomingBanner'
 
 const Home = ({ movies }) => {
   const title = <> Raw, Real & BTS from <br /> Salman Khan Films</>
   return (
     <>
       <HeroSection movies={movies} />
-      <UpcomingSection />
+      <UpcomingBanner/>
       <DirectorsSection />
       <Highlights tag={"highlight"} title={"News and Updates"} />
       <GallerySection title={title} />
