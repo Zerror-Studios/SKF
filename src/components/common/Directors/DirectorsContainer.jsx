896c42d4b5a64637767d46cdbe38f4aed5d9d65b
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-12-10 13:13:05 +0530

    updated cast card

--- src/components/common/Directors/DirectorsContainer.jsx
+++ src/components/common/Directors/DirectorsContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import DirectorCard from "./DirectorCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -7,9 +7,11 @@ import "swiper/css";
 import "swiper/css/free-mode";
 
 import { FreeMode } from "swiper/modules";
+import Cursor from "../Cursor";
 
 const DirectorsContainer = ({ data }) => {
   const [openIndex, setOpenIndex] = useState(null);
+  const sectionRef = useRef()
 
   useEffect(() => {
     if (window.innerWidth > 1286) {
@@ -21,28 +23,29 @@ const DirectorsContainer = ({ data }) => {
   const handleClose = () => setOpenIndex(null);
 
   return (
-    <div id="directors_container">
-     <Swiper
-  modules={[FreeMode]}
-  freeMode={true}
-  spaceBetween={20}
-  slidesPerView={"auto"}
-  grabCursor={true}
->
-  {data?.map((director, index) => (
-    <SwiperSlide key={index} style={{ width: "auto" }}>
-      <DirectorCard
-        data={director}
-        isOpen={openIndex === index}
-        onOpen={() => handleOpen(index)}
-        onClose={handleClose}
-      />
-    </SwiperSlide>
-  ))}
+    <div id="directors_container" ref={sectionRef}>
+      <Swiper
+        modules={[FreeMode]}
+        freeMode={true}
+        spaceBetween={20}
+        slidesPerView={"auto"}
+        grabCursor={true}
+      >
+        {data?.map((director, index) => (
+          <SwiperSlide key={index} style={{ width: "auto" }}>
+            <DirectorCard
+              data={director}
+              isOpen={openIndex === index}
+              onOpen={() => handleOpen(index)}
+              onClose={handleClose}
+            />
+          </SwiperSlide>
+        ))}
 
-  {/* Extra empty space card */}
-  <SwiperSlide style={{ width: "20vw" }}></SwiperSlide>
-</Swiper>
+        {/* Extra empty space card */}
+        <SwiperSlide style={{ width: "20vw" }}></SwiperSlide>
+      </Swiper>
+      <Cursor sectionRef={sectionRef} text="drag" />
 
     </div>
   );
