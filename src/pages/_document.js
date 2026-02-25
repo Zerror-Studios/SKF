3eabf078bac67244066be3a7dea5f749c32488d9
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2026-02-25 15:21:48 +0530

    Added GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KHFHTEQV2Z"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KHFHTEQV2Z');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
