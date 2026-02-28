e2c3c68439d4b3f1a03741f940d74bc43f81fd26
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2026-02-28 15:01:43 +0530

    updated redirect

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["img.youtube.com","www.youtube.com","cdn.sanity.io"], 
+    domains: ["img.youtube.com", "www.youtube.com", "cdn.sanity.io"],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'salmankhanfilms.com', // Your non-www domain
+          },
+        ],
+        destination: 'https://www.salmankhanfilms.com/:path*', // Your www domain
+        permanent: true, // Use permanent: true for a 301 redirect
+      },
+    ];
   },
 };
 
