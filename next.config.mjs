1837ae44fff61e1525f141f9224b985a7720da06
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2026-03-10 15:21:12 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     domains: ["img.youtube.com", "www.youtube.com", "cdn.sanity.io"],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/fonts/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/images/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+
   async redirects() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         has: [
           {
-            type: 'host',
-            value: 'salmankhanfilms.com', // Your non-www domain
+            type: "host",
+            value: "salmankhanfilms.com",
           },
         ],
-        destination: 'https://www.salmankhanfilms.com/:path*', // Your www domain
-        permanent: true, // Use permanent: true for a 301 redirect
+        destination: "https://www.salmankhanfilms.com/:path*",
+        permanent: true,
       },
     ];
   },
