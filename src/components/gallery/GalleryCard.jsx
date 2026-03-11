ab1a463760e1828b3cde04e7caa78dff16090752
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2026-03-11 16:28:21 +0530

    updated gallery card

--- src/components/gallery/GalleryCard.jsx
+++ src/components/gallery/GalleryCard.jsx
@@ -1,19 +1,47 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoArrowUpRight } from "react-icons/go";
 
 const GalleryCard = ({ slug, title, cover }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 480);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
   return (
-    <Link href={slug} style={{textDecoration:"none"}} className="gallery_wrap landing_text">
-      <div className="gallery_card cursor-pointer ">
-        {[1, 2, 3].map((num) => (
-          <div key={num} className={`image${num} image`}>
+    <Link
+      href={slug}
+      style={{ textDecoration: "none" }}
+      className="gallery_wrap landing_text"
+    >
+      <div className="gallery_card cursor-pointer">
+        {isMobile ? (
+          <div className="image3 image">
             {cover && (
               <Image width={1000} height={1000} src={cover} alt={title} />
             )}
           </div>
-        ))}
+        ) : (
+          [1, 2, 3].map((num) => (
+            <div key={num} className={`image${num} image`}>
+              {cover && (
+                <Image width={1000} height={1000} src={cover} alt={title} />
+              )}
+            </div>
+          ))
+        )}
+
         <div className="card_details">
           <h2 className="heading"></h2>
           <div className="arrow">
@@ -24,7 +52,8 @@ const GalleryCard = ({ slug, title, cover }) => {
           </div>
         </div>
       </div>
-      <div className="movie_dets" style={{marginTop:"1rem",color:"#000"}}>
+
+      <div className="movie_dets" style={{ marginTop: "1rem", color: "#000" }}>
         <span>{title}</span>
       </div>
     </Link>
